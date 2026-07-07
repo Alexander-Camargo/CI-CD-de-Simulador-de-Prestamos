2b4b6eb44d2941ad0aa2bfa58e5f703051cd162d
KidianJ <jorgekidian@gmail.com>
2026-07-07 17:27:46 -0500

    Update container.tf

--- terraform/container.tf
+++ terraform/container.tf
@@ -1,10 +1,14 @@
-# 1. Cuenta de Cosmos DB (Base de Datos NoSQL)
+# Cuenta de Cosmos DB (Base de Datos NoSQL)
 resource "azurerm_cosmosdb_account" "db" {
   name                = "${var.prefix}-cosmos-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 
   consistency_policy {
     consistency_level = "Session"
@@ -13,36 +17,5 @@ resource "azurerm_cosmosdb_account" "db" {
   geo_location {
     location          = var.location
     failover_priority = 0
-  }
-}
-
-# 2. Azure Container Instance (Donde correrá tu aplicación)
-resource "azurerm_container_group" "aci" {
-  name                = "${var.prefix}-aci-${var.environment}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_address_type = "Private"
-  os_type         = "Linux"
-
-  subnet_ids = [
-    azurerm_subnet.subnet.id
-  ]
-
-  container {
-    name   = "simulador-app"
-    image  = "${azurerm_container_registry.acr.login_server}/simulador-app:latest"
-
-    cpu    = 0.5
-    memory = 1.5
-
-    ports {
-      port     = 8080
-      protocol = "TCP"
-    }
-
-    secure_environment_variables = {
-      PORT = "8080"
-    }
   }
 }
