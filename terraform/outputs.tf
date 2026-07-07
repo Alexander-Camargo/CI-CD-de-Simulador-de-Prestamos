970626fb06d1266734e280c81f29e0bc4faf57ab
KidianJ <jorgekidian@gmail.com>
2026-07-07 17:31:07 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "aci_private_ip" {
-  value       = azurerm_container_group.aci.ip_address
-  description = "IP Privada de la aplicación desplegada en ACI"
-}
+# Outputs del proyecto
+
+# La IP del Azure Container Instance se habilitará en el Pipeline 3
+# cuando se despliegue la aplicación en ACI.
